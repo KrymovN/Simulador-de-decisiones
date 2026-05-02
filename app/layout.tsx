6b80c5253929e37cbdbc05329230df4676e97c66
S 3 <s3@MacBook-Air-S.local>
2026-05-02 20:51:28 +0200

    force update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
-import type { PropsWithChildren } from "react";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  console.log("layout updated"); // 👈 добавь это
   return (
     <html lang="en">
       <body>{children}</body>
